ec2088865d723e04366474f87341f61091199e2b
Chris Hartjes <chartjes@littlehart.net>
2018-11-08 09:53:45 -0500

    Removed tests that were deemed not necessary, added test that checks to make sure recipe implementation URL's generate a 200 when loaded.

--- api_tests.py
+++ api_tests.py
@@ -2,141 +2,59 @@ import os
 from molotov import scenario
 
 
-@scenario(weight=8)
-async def api_endpoint_test(session):
-    async with session.get(os.environ["NORMANDY_API_ENDPOINT"]) as resp:
-        res = await resp.json()
-        assert resp.status == 200
-        assert "action-list" in res
-        assert "action-signed" in res
-        assert "recipe-list" in res
-        assert "recipe-signed" in res
-        assert "reciperevision-list" in res
-        assert "approvalrequest-list" in res
-        assert "classify-client" in res
-
-
 @scenario(weight=7)
 async def recipe_endpoint_test(session):
-    recipe_endpoint = os.environ["NORMANDY_API_ENDPOINT"] + "recipe/?enabled=true"
-    fields = [
-        "id",
-        "last_updated",
-        "name",
-        "enabled",
-        "is_approved",
-        "revision_id",
-        "action",
-        "arguments",
-        "extra_filter_expression",
-        "filter_expression",
-        "latest_revision_id",
-        "approved_revision_id",
-        "approval_request",
-        "identicon_seed",
-    ]
+    recipe_endpoint = os.environ["NORMANDY_DOMAIN"] + "/api/v1/recipe/?enabled=true/"
     async with session.get(recipe_endpoint) as resp:
         res = await resp.json()
         assert resp.status == 200
         assert len(res) > 0
-        for field in fields:
-            assert field in res[0]
 
 
 @scenario(weight=7)
 async def signed_recipe_endpoint_test(session):
-    recipe_endpoint = (
-        os.environ["NORMANDY_API_ENDPOINT"] + "recipe/signed/?enabled=true"
+    signed_recipe_endpoint = (
+        os.environ["NORMANDY_DOMAIN"] + "/api/v1/recipe/signed/?enabled=true/"
     )
-    signature_fields = ["timestamp", "signature", "x5u", "public_key"]
-    recipe_fields = [
-        "id",
-        "name",
-        "revision_id",
-        "action",
-        "arguments",
-        "filter_expression",
-    ]
-    async with session.get(recipe_endpoint) as resp:
+    async with session.get(signed_recipe_endpoint) as resp:
         res = await resp.json()
         assert resp.status == 200
         assert len(res) > 0
-        assert "signature" in res[0]
-        assert "recipe" in res[0]
-
-        for field in signature_fields:
-            assert field in res[0]["signature"]
-
-        for field in recipe_fields:
-            assert field in res[0]["recipe"]
 
 
 @scenario(weight=7)
-async def signed_action_endpoint_test(session):
-    action_endpoint = os.environ["NORMANDY_API_ENDPOINT"] + "action/signed"
-    signature_fields = ["timestamp", "signature", "x5u", "public_key"]
-    action_fields = ["name", "implementation_url", "arguments_schema"]
-    async with session.get(action_endpoint) as resp:
+async def heartbeat_test(session):
+    heartbeat_endpoint = os.environ["NORMANDY_DOMAIN"] + "/__heartbeat__"
+    async with session.get(heartbeat_endpoint) as resp:
         res = await resp.json()
         assert resp.status == 200
-        assert len(res) > 0
-        assert "signature" in res[0]
-        assert "action" in res[0]
-
-        for field in signature_fields:
-            assert field in res[0]["signature"]
-
-        for field in action_fields:
-            assert field in res[0]["action"]
-
-
-@scenario(weight=7)
-async def action_endpoint_test(session):
-    action_endpoint = os.environ["NORMANDY_API_ENDPOINT"] + "action"
-    arguments_schema_fields = ["$schema", "title", "type", "required", "properties"]
-    async with session.get(action_endpoint) as resp:
-        res = await resp.json()
-        assert resp.status == 200
-        assert len(res) > 0
-        assert "name" in res[0]
-        assert "implementation_url" in res[0]
-        assert "arguments_schema" in res[0]
-
-        for field in arguments_schema_fields:
-            assert field in res[0]["arguments_schema"]
-
-
-@scenario(weight=7)
-async def recipe_revision_test(session):
-    endpoint = os.environ["NORMANDY_API_ENDPOINT"] + "recipe_revision"
-    fields = ["id", "date_created", "recipe", "comment", "approval_request"]
-    async with session.get(endpoint) as resp:
-        res = await resp.json()
-        assert resp.status == 200
-        assert len(res) > 0
-
-        for field in fields:
-            assert field in res[0]
-
-
-@scenario(weight=7)
-async def approval_request_test(session):
-    endpoint = os.environ["NORMANDY_API_ENDPOINT"] + "approval_request"
-    fields = ["id", "created", "approved", "approver", "comment"]
-    async with session.get(endpoint) as resp:
-        res = await resp.json()
-        assert resp.status == 200
-        assert len(res) > 0
-
-        for field in fields:
-            assert field in res[0]
+        assert "status" in res
 
 
 @scenario(weight=50)
 async def classify_client_test(session):
-    classify_client_endpoint = os.environ["NORMANDY_API_ENDPOINT"] + "classify_client"
+    classify_client_endpoint = (
+        os.environ["NORMANDY_DOMAIN"] + "/api/v1/classify_client/"
+    )
     async with session.get(classify_client_endpoint) as resp:
         res = await resp.json()
         assert resp.status == 200
         assert "country" in res
         assert "request_time" in res
+
+
+@scenario(weight=7)
+async def implementation_url_tests(session):
+    signed_action_endpoint = os.environ["NORMANDY_DOMAIN"] + "/api/v1/action/signed/"
+    async with session.get(signed_action_endpoint) as resp:
+        res = await resp.json()
+        assert resp.status == 200
+        count = 0
+
+        while count <= 4 and res[count]["action"]["implementation_url"] is not None:
+            implementation_url = res[count]["action"]["implementation_url"]
+            async with session.get(implementation_url) as iu_resp:
+                iu_res = await iu_resp.text()
+                assert iu_resp.status == 200
+                assert len(iu_res) > 0
+                count = count + 1
